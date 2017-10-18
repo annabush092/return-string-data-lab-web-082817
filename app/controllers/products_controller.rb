20677d528c3aa6c44cb27b5d5ce5880981bd29fd
annabush092 <annabush092@gmail.com>
2017-10-18 12:17:34 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,34 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(name: params[:product][:name], price: params[:product][:price].to_i, description: params[:product][:description], inventory: params[:product][:inventory].to_i)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def description
+    @product = Product.find_by(id: params[:id])
+    render plain: @product.description
+  end
+
+  def inventory
+    @product = Product.find_by(id: params[:id])
+    if !!(@product.inventory) && @product.inventory > 0
+      render plain: "true"
+    else
+      render plain: "false"
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
+# products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
+# 10.times do
+#   products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
+#   Product.create(:name => products.sample, :price => Faker::Number.between(1,1500))
+# end
+# 5.times do
+#   Customer.create(:name => Faker::Name.name)
+# end
+#
+# 5.times do
+#   Invoice.create
+# end
+# 10.times do
+#   order = Order.create(:customer_id => Customer.all.collect(&:id).sample, :invoice_id => Invoice.all.collect(&:id).sample)
+#   order.products << Product.find(Product.all.collect(&:id).sample)
+# end
 
-10.times do
-  products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
-  Product.create(:name => products.sample, :price => Faker::Number.between(1,1500))
+Product.all.each do |p|
+  p.update(description: "To be, or not to be--that is the question:
+Whether 'tis nobler in the mind to suffer
+The slings and arrows of outrageous fortune
+Or to take arms against a sea of troubles
+And by opposing end them. To die, to sleep--
+No more--and by a sleep to say we end
+The heartache, and the thousand natural shocks
+That flesh is heir to. 'Tis a consummation
+Devoutly to be wished. To die, to sleep--
+To sleep--perchance to dream: ay, there's the rub,
+For in that sleep of death what dreams may come
+When we have shuffled off this mortal coil,
+Must give us pause. There's the respect
+That makes calamity of so long life.
+For who would bear the whips and scorns of time,
+Th' oppressor's wrong, the proud man's contumely
+The pangs of despised love, the law's delay,
+The insolence of office, and the spurns
+That patient merit of th' unworthy takes,
+When he himself might his quietus make
+With a bare bodkin? Who would fardels bear,
+To grunt and sweat under a weary life,
+But that the dread of something after death,
+The undiscovered country, from whose bourn
+No traveller returns, puzzles the will,
+And makes us rather bear those ills we have
+Than fly to others that we know not of?
+Thus conscience does make cowards of us all,
+And thus the native hue of resolution
+Is sicklied o'er with the pale cast of thought,
+And enterprise of great pitch and moment
+With this regard their currents turn awry
+And lose the name of action. -- Soft you now,
+The fair Ophelia! -- Nymph, in thy orisons
+Be all my sins remembered.")
 end
-5.times do
-  Customer.create(:name => Faker::Name.name)
-end
-
-5.times do
-  Invoice.create
-end
-10.times do
-  order = Order.create(:customer_id => Customer.all.collect(&:id).sample, :invoice_id => Invoice.all.collect(&:id).sample)
-  order.products << Product.find(Product.all.collect(&:id).sample)
-end
-
